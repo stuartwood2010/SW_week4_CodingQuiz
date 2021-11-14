872f748022bd20c0757eb923d55d84e251d3acb0
Stuart Wood <stuartwood2010@hotmail.com>
2021-11-14 09:24:44 -0700

    Answers populating to page

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
 const myQuestions = [
-    question1 = { 
-        question: "What is Javascript used for?", 
+    {   question: "What is Javascript used for?", 
         answers: [
                 "JavaScript gives structure to a web page.", "JavaScript gives style to a web page.",
                 "JavaScript gives interactive elements to a web page.", 
                 "Javascript is the name that web developers give to coffee."
                 ],
         correctAnswer: "JavaScript gives interactive elements to a web page."
-    },
-    
-    question2 = {
-        question: "Which of the following is NOT a Javascript data type?",        
+    },    
+    {   question: "Which of the following is NOT a Javascript data type?",        
         answers: ["Boolean", "Object", "String", "Boomerang"],        
         correctAnswer: "Boomerang",
-    },
-    
-    question3 = {
-        question: "What is the code used for strict equality?",        
+    },    
+    {   question: "What is the code used for strict equality?",        
         answers: ["===", "!==", "="],        
         correctAnswer: "==="
     }
 ];
+const welcomeEl = $('#welcome');
 const startBtnEl = $('#startButton');
 const leaderBtnEl = $('#leaderboardButton');
 const gameEl = $('#game');
@@ -29,26 +25,36 @@ const timerEl = $("#timer");
 timerEl.hide();
 /*The timer will show and the welcome message will be hidden when the quiz starts*/
 /*And the first question will appear on the screen*/
+let questionEl = $('<h2>');
+questionEl.attr('id', 'questionEl');
+let answersEl = $('<ul>');
+let answerEl1 = $('<div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"><label class="form-check-label" for="flexRadioDefault1">""</label></div>');
+let answerEl2 = $('<div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"><label class="form-check-label" for="flexRadioDefault1">""</label></div>');
+let answerEl3 = $('<div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3"><label class="form-check-label" for="flexRadioDefault1">""</label></div>');
+let answerEl4 = $('<div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4"><label class="form-check-label" for="flexRadioDefault1">""</label></div>');
+answersEl.attr('class', 'answers');
 function startQuiz() { 
     countdown()
     timerEl.show();
     $('#welcome').hide();
-    let questionEl = $('<h2>');
-    currentQuestion = myQuestions[0];
+    let i = 0;
+    currentQuestion = myQuestions[i];
     questionEl.text(currentQuestion.question);
-    gameEl.append(questionEl);
-    let answersEl = $('ul');
-    let answerEl = $('li');
-    for (i=0; i < currentQuestion.answerslength; i++) {
-        answerEl.text(currentQuestion.answers[0])
-        // answersEl.append(answerEl);
-        // gameEl.append(answersEl);
-        console.log(answerEl);
-    }
-}
+    gameEl.append(questionEl);    
+    answerEl1.text(currentQuestion.answers[0]);
+    answerEl2.text(currentQuestion.answers[1]);
+    answerEl3.text(currentQuestion.answers[2]);
+    answerEl4.text(currentQuestion.answers[3]);
+    answersEl.append(answerEl1);
+    answersEl.append(answerEl2);
+    answersEl.append(answerEl3);
+    answersEl.append(answerEl4);
+    gameEl.append(answersEl);
+}      
+ 
 /*The timer will countdown from 30, with every wrong answer the user gets 10 seconds deducted*/
 /*Every right answer the timer will add 10 seconds; the time left on the clock when the quiz ends is the score*/
-let secondsLeft = 2;
+let secondsLeft = 25;
 function countdown(){
     let timerInterval = setInterval(function() {
         if(secondsLeft > 1) {
@@ -61,15 +67,11 @@ function countdown(){
             timerEl.text('');
             clearInterval(timerInterval);
             leaderboard();
-            // nextQuestion();
-         }    
+        }    
     }, 1000);
 }
 // function nextQuestion() {
-//     for (i=0; i < myQuestions.length; i++) {
-//         console.log("Question " + myQuestions[i].question);
-//     }
-// }
+
 
 function leaderboard() {
     timerEl.hide();

0b192326cf69a4eae790adcd754f95d15b4299f8
Stuart Wood <stuartwood2010@hotmail.com>
2021-11-16 21:56:48 -0700

    Questions and Answers are cycling through

--- js/script.js
+++ js/script.js
@@ -1,46 +1,65 @@
-const myQuestions = [
+let myQuestions = [
     {   question: "What is Javascript used for?", 
         answers: [
-                "JavaScript gives structure to a web page.", "JavaScript gives style to a web page.",
-                "JavaScript gives interactive elements to a web page.", 
-                "Javascript is the name that web developers give to coffee."
+                "A: JavaScript gives structure to a web page.",
+                "B: JavaScript gives style to a web page.",
+                "C: JavaScript gives interactive elements to a web page.", 
+                "D: Javascript is what web developers call coffee."
                 ],
-        correctAnswer: "JavaScript gives interactive elements to a web page."
     },    
     {   question: "Which of the following is NOT a Javascript data type?",        
-        answers: ["Boolean", "Object", "String", "Boomerang"],        
-        correctAnswer: "Boomerang",
+        answers: ["A: Boolean", "B: Object", "C: String", "D: Boomerang"],
     },    
     {   question: "What is the code used for strict equality?",        
-        answers: ["===", "!==", "="],        
-        correctAnswer: "==="
+        answers: ["A: ===", "B: !==", "C: =", "D: <=>"],
     }
 ];
 const welcomeEl = $('#welcome');
 const startBtnEl = $('#startButton');
 const leaderBtnEl = $('#leaderboardButton');
-const gameEl = $('#game');
-const timerEl = $("#timer");
-/*We are going to hide the timer until the quiz is started*/
+const btnMCEl = $('.butonsMC');
+const AbtnMCEl = $('#buttonA');
+const BbtnMCEl = $('#buttonB');
+const CbtnMCEl = $('#buttonC');
+const DbtnMCEl = $('#buttonD');
+let gameEl = $('#game');
+let timerEl = $("#timer");
+/*We are going to hide the timer and Multiple Coice buttons until the quiz is started*/
 timerEl.hide();
+AbtnMCEl.hide();
+BbtnMCEl.hide();
+CbtnMCEl.hide();
+DbtnMCEl.hide();
+let questionEl = $('<h2>');
+let answersEl = $('<ul>');
+let answerEl1 = $('<li>');
+let answerEl2 = $('<li>');
+let answerEl3 = $('<li>');
+let answerEl4 = $('<li>');
+let i = 0;
+let currentQuestion=myQuestions[i]
+answersEl.attr('class', 'answers');
+questionEl.attr('id', 'questionEl');
 /*The timer will show and the welcome message will be hidden when the quiz starts*/
 /*And the first question will appear on the screen*/
-let questionEl = $('<h2>');
-questionEl.attr('id', 'questionEl');
-let answersEl = $('<ul>');
-let answerEl1 = $('<div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"><label class="form-check-label" for="flexRadioDefault1">""</label></div>');
-let answerEl2 = $('<div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"><label class="form-check-label" for="flexRadioDefault1">""</label></div>');
-let answerEl3 = $('<div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3"><label class="form-check-label" for="flexRadioDefault1">""</label></div>');
-let answerEl4 = $('<div class="form-check"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4"><label class="form-check-label" for="flexRadioDefault1">""</label></div>');
-answersEl.attr('class', 'answers');
 function startQuiz() { 
     countdown()
-    timerEl.show();
     $('#welcome').hide();
-    let i = 0;
-    currentQuestion = myQuestions[i];
+    AbtnMCEl.show();
+    BbtnMCEl.show();
+    CbtnMCEl.show();
+    DbtnMCEl.show();
+    nextQuestion();
+}      
+function nextQuestion(){
+    console.log('I am happening');
+    let currentQuestion=myQuestions[i];
+    if (i > myQuestions.length -1) {
+        leaderboard();
+        return
+    }   
     questionEl.text(currentQuestion.question);
-    gameEl.append(questionEl);    
+    gameEl.append(questionEl);
     answerEl1.text(currentQuestion.answers[0]);
     answerEl2.text(currentQuestion.answers[1]);
     answerEl3.text(currentQuestion.answers[2]);
@@ -50,12 +69,12 @@ function startQuiz() {
     answersEl.append(answerEl3);
     answersEl.append(answerEl4);
     gameEl.append(answersEl);
-}      
- 
-/*The timer will countdown from 30, with every wrong answer the user gets 10 seconds deducted*/
-/*Every right answer the timer will add 10 seconds; the time left on the clock when the quiz ends is the score*/
-let secondsLeft = 25;
+    i++; 
+}
+/*The timer will countdown from 60*/
+let secondsLeft = 60;
 function countdown(){
+    timerEl.show();
     let timerInterval = setInterval(function() {
         if(secondsLeft > 1) {
             timerEl.text(secondsLeft + ' seconds remaining');
@@ -70,12 +89,19 @@ function countdown(){
         }    
     }, 1000);
 }
-// function nextQuestion() {
-
-
 function leaderboard() {
     timerEl.hide();
-    gameEl.hide();    
+    welcomeEl.hide();
+    gameEl.hide();  
+    AbtnMCEl.hide();
+    BbtnMCEl.hide();
+    CbtnMCEl.hide();
+    DbtnMCEl.hide();  
 }
 /*Start the quiz when the user clicks on the startButton*/
 startBtnEl.on("click", startQuiz);
+leaderBtnEl.on("click", leaderboard);
+AbtnMCEl.on("click", nextQuestion);
+BbtnMCEl.on("click", nextQuestion);
+CbtnMCEl.on("click", nextQuestion);
+DbtnMCEl.on("click", nextQuestion);
